16f5a3ec2cb6994540b5573a7f99cfc87f229165
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-20 14:54:40 -0500

    Update Q4.cs

--- Q4.cs
+++ Q4.cs
@@ -1,21 +1,2 @@
 ﻿using System;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-namespace RegexTask{
-class Q4{
-        static void Main(string[] args)
-        {
-            string[] testCases = { "Suuu$21g@", "W#1g@", "a&&g@", "sdsd723#$Amid", "sdsd723#$Amidkiouy" };
-            foreach (string text in testCases)
-                Console.WriteLine($"Original string: {text} is a valid password? {PasswordCheck(text)}\n");
-        }
-        public static bool PasswordCheck(string text) =>
-            text.Length >= 7 && text.Length <= 16 &&
-            Regex.IsMatch(text, "[A-Z]") &&
-            Regex.IsMatch(text, "[a-z]") &&
-            Regex.IsMatch(text, @"\d") &&
-            Regex.IsMatch(text, @"[!-/:-@\[-_{-~]") &&
-            !Regex.IsMatch(text, @"[^\dA-Za-z!-/:-@\[-_{-~]");
-    }
-}
+

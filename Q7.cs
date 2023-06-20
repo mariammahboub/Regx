b5e204a4f7c4a20e3b1ca9517c41d71e501b8fed
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-20 16:08:52 -0500

    Q7.cs

--- Q7.cs
+++ Q7.cs
@@ -5,19 +5,7 @@ class Q7
 {
         static void Main()
         {
-            string[] testCases = {
-            "$10.99",
-            "€15.50",
-            "£25",
-            "100.00",
-            "CAD 50.75",
-            "12.345.67",
-            "abc",
-            "USD 20.50$",
-            "$ 50",
-            "€-10.99"
-        };
-
+            string[] testCases = {"$10.99","10#","av10","100","103"};
             foreach (string testCase in testCases)
             {
                 bool isValidCurrency = Regex.IsMatch(testCase, @"^([A-Z]{3}\s*)?(-?\$|€|£)?\d{1,3}(,\d{3})*(\.\d{2})?$");

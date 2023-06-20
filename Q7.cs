c7f44fba843a9cccdd0f3c58ac091be8a8ca5791
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-20 16:33:52 -0500

    Q7.cs

--- Q7.cs
+++ Q7.cs
@@ -5,7 +5,7 @@ class Q7
 {
         static void Main()
         {
-            string[] testCases = {"$10.99","10#","av10","100","103"};
+            string[] testCases = {"$10.99","10#","av10","100","£103"};
             foreach (string testCase in testCases)
             {
                 bool isValidCurrency = Regex.IsMatch(testCase, @"^\p{Sc}?\d+(,\d{3})*(\.\d{2})?$");//\p{Sc} is a Unicode category that represents currency symbol
